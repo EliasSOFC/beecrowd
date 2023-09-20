b1b84dff8539dd1641f92a543f98d480cf091b52
Elias S <142616607+EliasSOFC@users.noreply.github.com>
2023-09-20 02:22:29 -0300

    Intervalo

---
+++ iniciante/Intervalo.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Intervalo {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double valor = sc.nextDouble();
+        
+        if (valor >= 0 && valor <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (valor > 25 && valor <= 50) {
+            System.out.println("Intervalo (25,50]");
+        } else if (valor > 50 && valor <= 75) {
+            System.out.println("Intervalo (50,75]");
+        } else if (valor > 75 && valor <= 100) {
+            System.out.println("Intervalo (75,100]");
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+
+        sc.close();
+ 
+    }
+ 
+}
